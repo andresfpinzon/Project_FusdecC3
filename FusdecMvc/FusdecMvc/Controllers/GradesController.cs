40d906c4b60d356ca13c4db8bfece5f2c7574e43
SantiagoRYS <152504184+SantiagoRYS@users.noreply.github.com>
2024-09-07 22:51:31 -0500

    feature(back-end): Agregamos el controlador para Calificacion junto con sus vistas

---
+++ FusdecMvc/FusdecMvc/Controllers/GradesController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FusdecMvc.Data;
+using FusdecMvc.Models;
+
+namespace FusdecMvc.Controllers
+{
+    public class GradesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GradesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Grades
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Grades.Include(g => g.Report);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Grades/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var grade = await _context.Grades
+                .Include(g => g.Report)
+                .FirstOrDefaultAsync(m => m.IdGrade == id);
+            if (grade == null)
+            {
+                return NotFound();
+            }
+
+            return View(grade);
+        }
+
+        // GET: Grades/Create
+        public IActionResult Create()
+        {
+            ViewData["IdReport"] = new SelectList(_context.Reports, "IdReport", "IdReport");
+            return View();
+        }
+
+        // POST: Grades/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdGrade,IdReport,Approved,ObservationGrade")] Grade grade)
+        {
+            //if (ModelState.IsValid)
+            {
+                grade.IdGrade = Guid.NewGuid();
+                _context.Add(grade);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdReport"] = new SelectList(_context.Reports, "IdReport", "IdReport", grade.IdReport);
+            return View(grade);
+        }
+
+        // GET: Grades/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var grade = await _context.Grades.FindAsync(id);
+            if (grade == null)
+            {
+                return NotFound();
+            }
+            ViewData["IdReport"] = new SelectList(_context.Reports, "IdReport", "IdReport", grade.IdReport);
+            return View(grade);
+        }
+
+        // POST: Grades/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("IdGrade,IdReport,Approved,ObservationGrade")] Grade grade)
+        {
+            if (id != grade.IdGrade)
+            {
+                return NotFound();
+            }
+
+            //if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(grade);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!GradeExists(grade.IdGrade))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdReport"] = new SelectList(_context.Reports, "IdReport", "IdReport", grade.IdReport);
+            return View(grade);
+        }
+
+        // GET: Grades/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var grade = await _context.Grades
+                .Include(g => g.Report)
+                .FirstOrDefaultAsync(m => m.IdGrade == id);
+            if (grade == null)
+            {
+                return NotFound();
+            }
+
+            return View(grade);
+        }
+
+        // POST: Grades/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var grade = await _context.Grades.FindAsync(id);
+            if (grade != null)
+            {
+                _context.Grades.Remove(grade);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool GradeExists(Guid id)
+        {
+            return _context.Grades.Any(e => e.IdGrade == id);
+        }
+    }
+}
