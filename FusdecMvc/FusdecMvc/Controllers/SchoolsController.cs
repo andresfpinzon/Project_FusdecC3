b1088ab0089e8681119c6f4665cf505c52f2f662
sebastianpinzon19 <152411485+sebastianpinzon19@users.noreply.github.com>
2024-09-08 21:51:09 -0500

    feat(back-end): add Schools for views and controller Fusdec

---
+++ FusdecMvc/FusdecMvc/Controllers/SchoolsController.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FusdecMvc.Data;
+using FusdecMvc.Models;
+
+namespace FusdecMvc.Controllers
+{
+    public class SchoolsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SchoolsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Schools
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Schools.ToListAsync());
+        }
+
+        // GET: Schools/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var school = await _context.Schools
+                .FirstOrDefaultAsync(m => m.IdSchool == id);
+            if (school == null)
+            {
+                return NotFound();
+            }
+
+            return View(school);
+        }
+
+        // GET: Schools/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Schools/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdSchool,SchoolName,SchoolEmail")] School school)
+        {
+            if (ModelState.IsValid)
+            {
+                school.IdSchool = Guid.NewGuid();
+                _context.Add(school);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(school);
+        }
+
+        // GET: Schools/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var school = await _context.Schools.FindAsync(id);
+            if (school == null)
+            {
+                return NotFound();
+            }
+            return View(school);
+        }
+
+        // POST: Schools/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("IdSchool,SchoolName,SchoolEmail")] School school)
+        {
+            if (id != school.IdSchool)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(school);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SchoolExists(school.IdSchool))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(school);
+        }
+
+        // GET: Schools/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var school = await _context.Schools
+                .FirstOrDefaultAsync(m => m.IdSchool == id);
+            if (school == null)
+            {
+                return NotFound();
+            }
+
+            return View(school);
+        }
+
+        // POST: Schools/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var school = await _context.Schools.FindAsync(id);
+            if (school != null)
+            {
+                _context.Schools.Remove(school);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SchoolExists(Guid id)
+        {
+            return _context.Schools.Any(e => e.IdSchool == id);
+        }
+    }
+}
