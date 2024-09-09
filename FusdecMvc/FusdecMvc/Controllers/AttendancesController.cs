39cb225a180137ecdfaa4317fd2f077da23cf768
SantiagoRYS <152504184+SantiagoRYS@users.noreply.github.com>
2024-09-08 23:39:06 -0500

    feature(back-end)Agregamos el controlador para la asistencia junto con sus vistas

---
+++ FusdecMvc/FusdecMvc/Controllers/AttendancesController.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FusdecMvc.Data;
+using FusdecMvc.Models;
+
+namespace FusdecMvc.Controllers
+{
+    public class AttendancesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AttendancesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Attendances
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Attendances.ToListAsync());
+        }
+
+        // GET: Attendances/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var attendance = await _context.Attendances
+                .FirstOrDefaultAsync(m => m.IdAttendance == id);
+            if (attendance == null)
+            {
+                return NotFound();
+            }
+
+            return View(attendance);
+        }
+
+        // GET: Attendances/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Attendances/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdAttendance,AttendanceDate,AttendanceStatus")] Attendance attendance)
+        {
+            if (ModelState.IsValid)
+            {
+                attendance.IdAttendance = Guid.NewGuid();
+                _context.Add(attendance);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(attendance);
+        }
+
+        // GET: Attendances/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var attendance = await _context.Attendances.FindAsync(id);
+            if (attendance == null)
+            {
+                return NotFound();
+            }
+            return View(attendance);
+        }
+
+        // POST: Attendances/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("IdAttendance,AttendanceDate,AttendanceStatus")] Attendance attendance)
+        {
+            if (id != attendance.IdAttendance)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(attendance);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AttendanceExists(attendance.IdAttendance))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(attendance);
+        }
+
+        // GET: Attendances/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var attendance = await _context.Attendances
+                .FirstOrDefaultAsync(m => m.IdAttendance == id);
+            if (attendance == null)
+            {
+                return NotFound();
+            }
+
+            return View(attendance);
+        }
+
+        // POST: Attendances/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var attendance = await _context.Attendances.FindAsync(id);
+            if (attendance != null)
+            {
+                _context.Attendances.Remove(attendance);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AttendanceExists(Guid id)
+        {
+            return _context.Attendances.Any(e => e.IdAttendance == id);
+        }
+    }
+}
