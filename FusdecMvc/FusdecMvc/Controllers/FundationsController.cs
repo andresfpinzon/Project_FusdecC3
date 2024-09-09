20dc5a6494343854acb5f1a999f044e03c4e14f9
sebastianpinzon19 <152411485+sebastianpinzon19@users.noreply.github.com>
2024-09-08 19:11:24 -0500

    feat(back-end): add fundations for view Fusdec

---
+++ FusdecMvc/FusdecMvc/Controllers/FundationsController.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FusdecMvc.Data;
+using FusdecMvc.Models;
+
+namespace FusdecMvc.Controllers
+{
+    public class FundationsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public FundationsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Fundations
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Fundations.ToListAsync());
+        }
+
+        // GET: Fundations/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var fundation = await _context.Fundations
+                .FirstOrDefaultAsync(m => m.IdFundation == id);
+            if (fundation == null)
+            {
+                return NotFound();
+            }
+
+            return View(fundation);
+        }
+
+        // GET: Fundations/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Fundations/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdFundation,FundationName")] Fundation fundation)
+        {
+            if (ModelState.IsValid)
+            {
+                fundation.IdFundation = Guid.NewGuid();
+                _context.Add(fundation);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(fundation);
+        }
+
+        // GET: Fundations/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var fundation = await _context.Fundations.FindAsync(id);
+            if (fundation == null)
+            {
+                return NotFound();
+            }
+            return View(fundation);
+        }
+
+        // POST: Fundations/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("IdFundation,FundationName")] Fundation fundation)
+        {
+            if (id != fundation.IdFundation)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(fundation);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!FundationExists(fundation.IdFundation))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(fundation);
+        }
+
+        // GET: Fundations/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var fundation = await _context.Fundations
+                .FirstOrDefaultAsync(m => m.IdFundation == id);
+            if (fundation == null)
+            {
+                return NotFound();
+            }
+
+            return View(fundation);
+        }
+
+        // POST: Fundations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var fundation = await _context.Fundations.FindAsync(id);
+            if (fundation != null)
+            {
+                _context.Fundations.Remove(fundation);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool FundationExists(Guid id)
+        {
+            return _context.Fundations.Any(e => e.IdFundation == id);
+        }
+    }
+}
