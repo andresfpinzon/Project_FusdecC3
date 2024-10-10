a30227046924b9c64dbf29738ecd8108152e64a7
Andres <andrespinzonxd@gmail.com>
2024-10-10 08:25:18 -0500

    feat(back-end): agregar modelo para usuario

---
+++ FusdecC3-BackEnd-Node/models/usuario_model.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const UsuarioSchema = new Schema({
+  _id: {
+    type: Schema.Types.ObjectId,
+    default: () => new mongoose.Types.ObjectId(),
+  },
+  nombreUsuario: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  correo: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  contraseñaHash: {
+    type: String,
+    required: true,
+  },
+  roles: [{
+      type: Schema.Types.ObjectId,
+      ref: "rol",
+    }],
+  creadoEn: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Usuario = mongoose.model("Usuario", UsuarioSchema);
+module.exports = Usuario;
