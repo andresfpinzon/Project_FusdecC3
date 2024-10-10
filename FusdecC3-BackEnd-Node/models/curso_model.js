bd8890dc6467db7babeb160f78df9b07864dfe0f
fepo12 <flugotique7895@gimail.com>
2024-10-10 15:18:48 -0500

    feat(backend): agregar modelo de curso

---
+++ FusdecC3-BackEnd-Node/models/curso_model.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const cursoSchema = new Schema({
+  CursoId: {
+    type: Schema.Types.ObjectId,
+    default: () => new mongoose.Types.ObjectId(),
+  },
+  nombreCurso: {
+    type: String,
+    required: true,
+  },
+  descripcionCurso: {
+    type: String,
+  },
+  intensidadHorariaCurso: {
+    type: String,
+    required: true,
+  },
+  estatusCurso: {
+    type: Boolean,
+    default: true,
+  },
+  fundacionId: {
+    type: Schema.Types.ObjectId,
+    ref: "Fundacion", // Referencia a la colección de Fundacion
+  },
+  ediciones: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Edicion", // Referencia a la colección de Edicion
+    },
+  ],
+  certificados: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Certificado", // Referencia a la colección de Certificado
+    },
+  ],
+});
+
+const Curso = mongoose.model("Curso", cursoSchema);
+
+module.exports = Curso;
