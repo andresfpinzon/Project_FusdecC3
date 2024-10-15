486e4141f4d77e19a1b3ce286420dd616865689f
SantiagoRYS <santiagorys2003@gmail.com>
2024-10-15 16:41:31 -0500

    feat(backend): Agregar logica para horario

--- FusdecC3-BackEnd-Node/logic/curso_logic.js
+++ FusdecC3-BackEnd-Node/logic/curso_logic.js
@@ -1,75 +1,87 @@
-const Curso = require('../models/curso_model');
+const Curso = require("../models/curso_model");
 
 // Función asíncrona para crear cursos
 async function crearCurso(body) {
-    // Verificar si ya existe un curso con el mismo título
-    const cursoExistente = await Curso.findOne({ nombreCurso: body.nombreCurso });
-    if (cursoExistente) {
-      throw new Error('El curso con este título ya existe');
-    }
-  
-    let curso = new Curso({
-        nombreCurso: body.nombreCurso,
-        descripcionCurso: body.descripcionCurso,
-        intensidadHorariaCurso: body.intensidadHorariaCurso,
-        fundacionId: body.fundacionId,
-        ediciones: body.ediciones
-    });
-  
-    return await curso.save();
+  // Verificar si ya existe un curso con el mismo título
+  const cursoExistente = await Curso.findOne({ nombreCurso: body.nombreCurso });
+  if (cursoExistente) {
+    throw new Error("El curso con este título ya existe");
+  }
+
+  let curso = new Curso({
+    nombreCurso: body.nombreCurso,
+    descripcionCurso: body.descripcionCurso,
+    intensidadHorariaCurso: body.intensidadHorariaCurso,
+    fundacionId: body.fundacionId,
+    ediciones: body.ediciones,
+  });
+
+  return await curso.save();
 }
 
- // Función asíncrona para actualizar cursos
+// Función asíncrona para actualizar cursos
 async function actualizarCurso(id, body) {
-    let curso = await Curso.findByIdAndUpdate(id, {
+  let curso = await Curso.findByIdAndUpdate(
+    id,
+    {
       $set: {
         nombreCurso: body.nombreCurso,
         descripcionCurso: body.descripcionCurso,
         intensidadHorariaCurso: body.intensidadHorariaCurso,
-        ediciones: body.ediciones
-      }
-    }, { new: true });
-  
-    return curso;
+        ediciones: body.ediciones,
+      },
+    },
+    { new: true }
+  );
+
+  return curso;
 }
 
 // Función asíncrona para inactivar cursos
 async function desactivarCurso(id) {
-    let curso = await Curso.findByIdAndUpdate(id, {
+  let curso = await Curso.findByIdAndUpdate(
+    id,
+    {
       $set: {
-        estadoCurso: false
-      }
-    }, { new: true });
-  
-    return curso;
+        estadoCurso: false,
+      },
+    },
+    { new: true }
+  );
+
+  return curso;
 }
-  
+
 // Función asíncrona para listar los cursos activos
 async function listarCursosActivos() {
-    let cursos = await Curso.find({ "estadoCurso": true });
-    return cursos;
+  let cursos = await Curso.find({ estadoCurso: true });
+  return cursos;
 }
-  
+
 // Función asíncrona para guardar una colección de cursos
 async function guardarCursos(cursos) {
-    try {
-      const resultados = [];
-      for (let cursoData of cursos) {
-        // Verificar si ya existe un curso con el mismo título
-        const cursoExistente = await Curso.findOne({ nombreCurso: body.nombreCurso });
-        if (!cursoExistente) {
-          let nuevoCurso = new Curso(cursoData);
-          let cursoGuardado = await nuevoCurso.save();
-          resultados.push(cursoGuardado);
-        } else {
-          console.log(`El curso con título "${cursoData.nombreCurso}" ya existe.`);
-        }
+  try {
+    const resultados = [];
+    for (let cursoData of cursos) {
+      // Verificar si ya existe un curso con el mismo título
+      const cursoExistente = await Curso.findOne({
+        nombreCurso: body.nombreCurso,
+      });
+      if (!cursoExistente) {
+        let nuevoCurso = new Curso(cursoData);
+        let cursoGuardado = await nuevoCurso.save();
+        resultados.push(cursoGuardado);
+      } else {
+        console.log(
+          `El curso con título "${cursoData.nombreCurso}" ya existe.`
+        );
       }
-      return resultados;
-    } catch (err) {
-      console.error('Error al guardar la colección de cursos:', err);
-      throw err; // Re-lanza el error para manejarlo en la capa superior si es necesario
     }
+    return resultados;
+  } catch (err) {
+    console.error("Error al guardar la colección de cursos:", err);
+    throw err; // Re-lanza el error para manejarlo en la capa superior si es necesario
+  }
 }
 
 // Función asíncrona para buscar un curso por su ID
@@ -87,10 +99,10 @@ async function buscarCursoPorId(id) {
 }
 
 module.exports = {
-    crearCurso,
-    actualizarCurso,
-    desactivarCurso,
-    listarCursosActivos,
-    guardarCursos,
-    buscarCursoPorId
-  };
+  crearCurso,
+  actualizarCurso,
+  desactivarCurso,
+  listarCursosActivos,
+  guardarCursos,
+  buscarCursoPorId,
+};
