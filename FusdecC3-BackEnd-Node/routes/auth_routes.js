4af5019ecb478939406ad25d0c3f678881a188e3
Andres <andrespinzonxd@gmail.com>
2024-10-21 11:11:35 -0500

    feat(back-end): agregar funcion que cargeu automaticamente las rutas de los endpoints

--- FusdecC3-BackEnd-Node/routes/auth_routes.js
+++ FusdecC3-BackEnd-Node/routes/auth_routes.js
@@ -1,11 +1,44 @@
 const express = require('express');
-const { login } = require('../controllers/auth_controllers'); // Ajusta la ruta según tu estructura de carpetas
-
 const router = express.Router();
+const { login } = require('../controllers/auth_controllers'); // Ajusta la ruta según tu estructura de carpetas
 
 /**
  * @swagger
- * /auth/login:
+ * components:
+ *   schemas:
+ *     AuthLogin:
+ *       type: object
+ *       required:
+ *         - correo
+ *         - contraseña
+ *       properties:
+ *         correo:
+ *           type: string
+ *           description: Correo del usuario
+ *           example: usuario@example.com
+ *         contraseña:
+ *           type: string
+ *           description: Contraseña del usuario
+ *           example: tu_contraseña
+ *     AuthTokenResponse:
+ *       type: object
+ *       properties:
+ *         token:
+ *           type: string
+ *           description: Token JWT generado
+ *           example: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...
+ *     ErrorResponse:
+ *       type: object
+ *       properties:
+ *         message:
+ *           type: string
+ *           description: Mensaje de error
+ *           example: 'Usuario no encontrado'
+ */
+
+/**
+ * @swagger
+ * /api/auth/login:
  *   post:
  *     summary: Iniciar sesión
  *     description: Permite a los usuarios iniciar sesión en la aplicación y obtener un token JWT.
--- FusdecC3-BackEnd-Node/routes/routesLoader.js
+++ FusdecC3-BackEnd-Node/routes/routesLoader.js
@@ -8,18 +8,18 @@ const pluralize = (word) => {
     if (lastChar === 'a' || lastChar === 'e' || lastChar === 'i' || lastChar === 'o' || lastChar === 'u') {
         return `${word}s`; // Agrega "s" si termina en vocal
     }
-    return `${word}s`; // Para consonantes, también agrega "s"
+    return `${word}es`; // Para consonantes, también agrega "es"
 };
 
 // Función para cargar las rutas dinámicamente
 const loadRoutes = (app) => {
-    const routesPath = path.join(__dirname, 'routes'); // Carpeta de rutas
+    const routesPath = __dirname; // Carpeta de rutas
 
     // Leer todos los archivos en la carpeta de rutas
     fs.readdirSync(routesPath).forEach((file) => {
         // Verificar que el archivo termine con '_routes.js'
         if (file.endsWith('_routes.js')) {
-            const route = `./routes/${file}`;
+            const route = path.join(routesPath, file);
             // Remover el sufijo '_routes.js' y usar solo la parte anterior para el nombre de la ruta
             const routeName = path.basename(file, '_routes.js');
             const cleanRouteName = routeName.replace('_routes', ''); // Elimina '_routes'
