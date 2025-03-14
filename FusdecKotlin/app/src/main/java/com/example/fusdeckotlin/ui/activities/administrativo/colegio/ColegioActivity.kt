70eb11a68b829ddf0dbbf8659be0959001346147
sebastianpinzon19 <sebastian_pinzonr@.sena.edu.co>
2025-03-14 11:22:09 -0500

    feat(kotlin): configurar xml brigada y colegio

--- FusdecKotlin/app/src/main/java/com/example/fusdeckotlin/ui/activities/administrativo/colegio/ColegioActivity.kt
+++ FusdecKotlin/app/src/main/java/com/example/fusdeckotlin/ui/activities/administrativo/colegio/ColegioActivity.kt
@@ -3,6 +3,7 @@ package com.example.fusdeckotlin.ui.activities.administrativo.colegio
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Switch
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.fusdeckotlin.R
@@ -12,7 +13,7 @@ class ColegioActivity : AppCompatActivity() {
 
     private lateinit var nombreColegioEditText: EditText
     private lateinit var emailColegioEditText: EditText
-    private lateinit var estadoColegioEditText: EditText
+    private lateinit var estadoColegioSwitch: Switch
     private lateinit var estudiantesEditText: EditText
     private lateinit var confirmarButton: Button
     private lateinit var cancelarButton: Button
@@ -23,7 +24,7 @@ class ColegioActivity : AppCompatActivity() {
 
         nombreColegioEditText = findViewById(R.id.nombreColegioEditText)
         emailColegioEditText = findViewById(R.id.emailColegioEditText)
-        estadoColegioEditText = findViewById(R.id.estadoColegioEditText)
+        estadoColegioSwitch = findViewById(R.id.estadoColegioSwitch)
         estudiantesEditText = findViewById(R.id.estudiantesEditText)
         confirmarButton = findViewById(R.id.confirmarButton)
         cancelarButton = findViewById(R.id.cancelarButton)
@@ -40,7 +41,7 @@ class ColegioActivity : AppCompatActivity() {
     private fun guardarColegio() {
         val nombreColegio = nombreColegioEditText.text.toString().trim()
         val emailColegio = emailColegioEditText.text.toString().trim()
-        val estadoColegio = estadoColegioEditText.text.toString().trim()
+        val estadoColegio = estadoColegioSwitch.isChecked.toString()
         val estudiantes = estudiantesEditText.text.toString().trim()
 
         if (nombreColegio.isEmpty() || emailColegio.isEmpty() || estadoColegio.isEmpty() || estudiantes.isEmpty()) {
