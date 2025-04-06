191326729a62349605f42caf5405d6c0b7e9d48b
Julian Rivera(VALHEMSING) <juli.rivera.quintero@gmail.com>
2025-04-06 16:28:45 -0500

    feat: Agrego los campos o valores para CreateBrigadaDto

--- FusdecKotlin/app/src/main/java/com/example/fusdeckotlin/dto/administrativo/brigada/CreateBrigadaDto.kt
+++ FusdecKotlin/app/src/main/java/com/example/fusdeckotlin/dto/administrativo/brigada/CreateBrigadaDto.kt
@@ -1,3 +1,20 @@
 package com.example.fusdeckotlin.dto.administrativo.brigada
 
-data class CreateBrigadaDto()
+import com.google.gson.annotations.SerializedName
+
+data class CreateBrigadaDto(
+    @SerializedName("nombreBrigada")
+    var nombreBrigada: String? = null,
+
+    @SerializedName("ubicacionBrigada")
+    var ubicacionBrigada: String? = null,
+
+    @SerializedName("estadoBrigada")
+     var estadoBrigada: Boolean? = null,
+
+    @SerializedName("comandoId")
+    var comandoId: String? = null,
+
+    @SerializedName("unidades")
+    private var unidades: List<String>? = null
+)
