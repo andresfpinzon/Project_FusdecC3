a10ca77fed7b60443bcf938f7c2474a5a35fc6a6
Julian Rivera <juli.rivera.quintero@gmail.com>
2025-04-07 16:30:16 -0500

    feat(backend): Agrego rutas y parametros para cada funcion, con la que actuara la ruta

--- FusdecKotlin/app/src/main/java/com/example/fusdeckotlin/api/administrativo/brigada/BrigadaApi.kt
+++ FusdecKotlin/app/src/main/java/com/example/fusdeckotlin/api/administrativo/brigada/BrigadaApi.kt
@@ -1,4 +1,28 @@
 package com.example.fusdeckotlin.api.administrativo.brigada
 
+import com.example.fusdeckotlin.dto.administrativo.brigada.CreateBrigadaDto
+import com.example.fusdeckotlin.dto.administrativo.brigada.UpdateBrigadaDto
+import com.example.fusdeckotlin.models.administrativo.brigada.Brigada
+import retrofit2.Response
+import retrofit2.http.*
+
 interface BrigadaApi {
+    @GET("api/brigadas")
+    suspend fun getBrigadas(): Response<List<Brigada>>
+
+    @POST("api/brigadas")
+    suspend fun createBrigada(@Body req: CreateBrigadaDto): Response<Brigada>
+
+    @PUT("api/brigadas/{id}")
+    suspend fun updateBrigada(
+        @Path("id") id: String,
+        @Body req: UpdateBrigadaDto
+    ): Response<Brigada>
+
+    @GET("api/brigadas/{id}")
+    suspend fun getBrigadaById(@Path("id") id: String): Response<Brigada>
+
+
+    @DELETE("api/brigadas/{id}")
+    suspend fun deleteBrigadaByid(@Path("id") id: String): Response<Brigada>
 }
