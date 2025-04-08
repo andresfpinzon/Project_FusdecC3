82843bcab27e88618e0242376f13354355f9ff4e
Julian Rivera(VALHEMSING) <juli.rivera.quintero@gmail.com>
2025-04-08 17:30:19 -0500

    feat: finalizado el dto para crear un comando

--- FusdecKotlin/app/src/main/java/com/example/fusdeckotlin/dto/administrativo/comando/CreateComandoDto.kt
+++ FusdecKotlin/app/src/main/java/com/example/fusdeckotlin/dto/administrativo/comando/CreateComandoDto.kt
@@ -3,5 +3,15 @@ package com.example.fusdeckotlin.dto.administrativo.comando
 import com.google.gson.annotations.SerializedName
 
 data class CreateComandoDto(
+    @SerializedName("nombreComando")
+    var nombreComando: String,
 
+    @SerializedName("ubicacionComando")
+    var ubicacionComando: String,
+
+    @SerializedName("fundacionId")
+    var fundacionId: String,
+
+    @SerializedName("brigadas")
+    var brigadas: List<String>
 )
