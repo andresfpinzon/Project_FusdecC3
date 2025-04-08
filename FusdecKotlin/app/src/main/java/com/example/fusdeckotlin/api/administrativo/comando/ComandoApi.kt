cd810a079e0a4dae7409c362ea622429861518d4
Julian Rivera(VALHEMSING) <juli.rivera.quintero@gmail.com>
2025-04-08 18:01:36 -0500

    feat: Interfaz de BrigadaApi finalizado con las rutas correspondientes

--- FusdecKotlin/app/src/main/java/com/example/fusdeckotlin/api/administrativo/comando/ComandoApi.kt
+++ FusdecKotlin/app/src/main/java/com/example/fusdeckotlin/api/administrativo/comando/ComandoApi.kt
@@ -1,4 +1,30 @@
 package com.example.fusdeckotlin.api.administrativo.comando
 
+import com.example.fusdeckotlin.dto.administrativo.comando.CreateComandoDto
+import com.example.fusdeckotlin.dto.administrativo.comando.UpdateComandoDto
+import com.example.fusdeckotlin.models.administrativo.comando.Comando
+import retrofit2.Response
+import retrofit2.http.*
+
 interface ComandoApi {
+    @GET("api/comandos")
+    suspend fun getComandos(): Response<List<Comando>>
+
+    @POST("api/comandos")
+    suspend fun createComando (@Body req: CreateComandoDto): Response<Comando>
+
+    @PUT("api/comandos/{id}")
+    suspend fun updateComando(
+        @Path("id") id: String,
+        @Body req: UpdateComandoDto
+    ): Response<Comando>
+
+    @GET("api/comandos/{id}")
+    suspend fun getComandoById(
+        @Path("id") id: String
+    ): Response<Comando>
+
+    @DELETE("api/comandos/{id}")
+    suspend fun  deleteComandoById(@Path("id") id: String): Response<Comando>
+
 }
