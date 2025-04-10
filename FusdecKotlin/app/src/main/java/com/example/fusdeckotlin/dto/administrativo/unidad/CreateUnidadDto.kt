ddc03045f5e7c37f0b6f4d366d1910530596133e
Julian Rivera(VALHEMSING) <juli.rivera.quintero@gmail.com>
2025-04-10 13:57:13 -0500

    fix: Modificacion de los campos del CreateUnidadDto eliminando la propiedad 'private'

--- FusdecKotlin/app/src/main/java/com/example/fusdeckotlin/dto/administrativo/unidad/CreateUnidadDto.kt
+++ FusdecKotlin/app/src/main/java/com/example/fusdeckotlin/dto/administrativo/unidad/CreateUnidadDto.kt
@@ -4,13 +4,13 @@ import com.google.gson.annotations.SerializedName
 
 data class CreateUnidadDto(
     @SerializedName("nombreUnidad")
-    private var nombreUnidad: String,
+    var nombreUnidad: String,
     @SerializedName("brigadaId")
-    private var brigadaId: String,
+    var brigadaId: String,
     @SerializedName("usuarioId")
-    private var usuarioId: String,
+    var usuarioId: String,
     @SerializedName("comandos")
-    private var comandos: List<String>,
+    var comandos: List<String>,
     @SerializedName("estudiantes")
-    private var estudiantes: List<String>
+    var estudiantes: List<String>
 )
