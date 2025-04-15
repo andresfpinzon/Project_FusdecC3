1aa20a3abeda1e438cc2b7ee17785bada4539ce9
Julian Rivera(VALHEMSING) <juli.rivera.quintero@gmail.com>
2025-04-15 17:19:36 -0500

    feat:  Archivo CreateAuditoriaDto, finalizado

--- FusdecKotlin/app/src/main/java/com/example/fusdeckotlin/dto/administrativo/auditoria/CreateAuditoriaDto.kt
+++ FusdecKotlin/app/src/main/java/com/example/fusdeckotlin/dto/administrativo/auditoria/CreateAuditoriaDto.kt
@@ -3,6 +3,7 @@ package com.example.fusdeckotlin.dto.administrativo.auditoria
 import com.google.gson.annotations.SerializedName
 
 data class CreateAuditoriaDto(
+    @SerializedName("nombreEmisor")
     private val nombreEmisor: String,
     @SerializedName("certificadoId")
     private val certificadoId: String,
