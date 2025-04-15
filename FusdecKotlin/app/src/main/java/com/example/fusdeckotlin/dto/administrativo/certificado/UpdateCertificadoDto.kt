f010bac287e5d7ae0641ef413632b94b7fda2ab9
Julian Rivera(VALHEMSING) <juli.rivera.quintero@gmail.com>
2025-04-15 18:56:18 -0500

    feat: UpdateCertificaDto finalizado

--- FusdecKotlin/app/src/main/java/com/example/fusdeckotlin/dto/administrativo/certificado/UpdateCertificadoDto.kt
+++ FusdecKotlin/app/src/main/java/com/example/fusdeckotlin/dto/administrativo/certificado/UpdateCertificadoDto.kt
@@ -1,5 +1,14 @@
 package com.example.fusdeckotlin.dto.administrativo.certificado
 
-data class UpdateCertificadoDto(
+import com.google.gson.annotations.SerializedName
 
+data class UpdateCertificadoDto(
+    @SerializedName("usuarioId")
+    private val usuarioId: String? = null,
+    @SerializedName("usuarioId")
+    private val cursoId: String? = null,
+    @SerializedName("estudianteId")
+    private val estudianteId: String? = null,
+    @SerializedName("nombreEmisorCertificado")
+    private var nombreEmisorCertificado: String? = null,
 )
