88d67efdd7fafdf4f812d53b7e9e97f2c86f0106
Julian Rivera(VALHEMSING) <juli.rivera.quintero@gmail.com>
2025-04-15 18:54:25 -0500

    feat: CreateCertificadoDto finalizado

--- FusdecKotlin/app/src/main/java/com/example/fusdeckotlin/dto/administrativo/certificado/CreateCertificadoDto.kt
+++ FusdecKotlin/app/src/main/java/com/example/fusdeckotlin/dto/administrativo/certificado/CreateCertificadoDto.kt
@@ -1,3 +1,14 @@
 package com.example.fusdeckotlin.dto.administrativo.certificado
 
-data class CreateCertificadoDto()
+import com.google.gson.annotations.SerializedName
+
+data class CreateCertificadoDto(
+    @SerializedName("usuarioId")
+    private val usuarioId: String,
+    @SerializedName("usuarioId")
+    private val cursoId: String,
+    @SerializedName("estudianteId")
+    private val estudianteId: String,
+    @SerializedName("nombreEmisorCertificado")
+    private var nombreEmisorCertificado: String,
+)
