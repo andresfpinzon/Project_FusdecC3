fe65e83e331dc4fb6a647c11ffd16b917c1ceab1
Julian Rivera(VALHEMSING) <juli.rivera.quintero@gmail.com>
2025-04-15 16:01:17 -0500

    feat: Interfaz de usuario con las rutas a usar finalizado

--- FusdecKotlin/app/src/main/java/com/example/fusdeckotlin/api/administrativo/user/IUserApi.kt
+++ FusdecKotlin/app/src/main/java/com/example/fusdeckotlin/api/administrativo/user/IUserApi.kt
@@ -1,5 +1,7 @@
 package com.example.fusdeckotlin.api.administrativo.user
 
+import com.example.fusdeckotlin.dto.administrativo.user.CreateUserDto
+import com.example.fusdeckotlin.dto.administrativo.user.UpdateUserDto
 import models.administrativo.user.model.Usuario
 import retrofit2.http.*
 import retrofit2.Response
@@ -9,5 +11,20 @@ interface IUserApi {
 
     @GET("api/usuarios")
     suspend fun getUsers(): Response<List<Usuario>>
+
+    @POST("api/usuarios")
+    suspend fun createUser(@Body data: CreateUserDto): Response<Usuario>
+
+    @PUT("api/usuarios/{id}")
+    suspend fun updateUser(
+        @Path("id") id: String,
+        @Body data: UpdateUserDto
+    ): Response<Usuario>
+
+    @GET("api/usuarios/{id}")
+    suspend fun getUserById(@Path("id") id: String): Response<Usuario>
+
+    @DELETE("api/usuarios/{id}")
+    suspend fun deleteUserById(@Path("id") id: String): Response<Usuario>
 
 }
