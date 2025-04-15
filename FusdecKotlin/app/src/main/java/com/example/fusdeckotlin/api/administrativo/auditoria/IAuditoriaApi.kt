680d390ce9828b447e606c24ce59a8760c929b9e
Julian Rivera(VALHEMSING) <juli.rivera.quintero@gmail.com>
2025-04-15 17:33:58 -0500

    feat: Agregue las rutas de auditoria

--- FusdecKotlin/app/src/main/java/com/example/fusdeckotlin/api/administrativo/auditoria/IAuditoriaApi.kt
+++ FusdecKotlin/app/src/main/java/com/example/fusdeckotlin/api/administrativo/auditoria/IAuditoriaApi.kt
@@ -1,4 +1,23 @@
 package com.example.fusdeckotlin.api.administrativo.auditoria
 
+import models.administrativo.auditoria.model.AuditoriaModel
+import retrofit2.http.*
+import retrofit2.Response
+
 interface IAuditoriaApi {
+
+    @GET("api/auditorias")
+    suspend fun getAuditorias(): Response<List<AuditoriaModel>>
+
+    @POST("api/auditorias")
+    suspend fun createAuditoria(): Response<AuditoriaModel>
+
+    @GET("api/auditorias/{id}")
+    suspend fun getAuditoriaById(@Path("id") id: String): Response<AuditoriaModel>
+
+    @GET("api/auditorias/certificado/{certificadoId}")
+    suspend fun getAuditoriaByCertificadoId(@Path("certificadoId") certificadoId: String): Response<AuditoriaModel>
+
+    @DELETE("api/auditorias/{id}")
+    suspend fun deleteAuditoriaById(@Path("id") id: String): Response<AuditoriaModel>
 }
