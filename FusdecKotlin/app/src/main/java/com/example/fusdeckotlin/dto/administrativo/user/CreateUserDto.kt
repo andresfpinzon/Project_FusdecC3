7d570f3ba5a10dfd5a90db1780ebf02c5762562f
Julian Rivera(VALHEMSING) <juli.rivera.quintero@gmail.com>
2025-04-18 15:58:03 -0500

    fix: modificacion de createUserDto

--- FusdecKotlin/app/src/main/java/com/example/fusdeckotlin/dto/administrativo/user/CreateUserDto.kt
+++ FusdecKotlin/app/src/main/java/com/example/fusdeckotlin/dto/administrativo/user/CreateUserDto.kt
@@ -3,16 +3,9 @@ package com.example.fusdeckotlin.dto.administrativo.user
 import com.google.gson.annotations.SerializedName
 
 data class CreateUserDto(
-    @SerializedName("nombreUsuario")
-    private var nombreUsuario: String,
-    @SerializedName("apellidoUsuario")
-    private var apellidoUsuario: String,
-    @SerializedName("numeroDocumento")
-    private var numeroDocumento: String,
-    @SerializedName("correo")
-    private var correo: String,
-    @SerializedName("password")
-    private var password: String,
-    @SerializedName("roles")
-    private var roles: List<String>? = null,
+    @SerializedName("numero_documento") private var numeroDocumento: String,
+    @SerializedName("nombre") private var nombre: String,
+    @SerializedName("apellido") private var apellido: String,
+    @SerializedName("correo") private var correo: String,
+    @SerializedName("password") private var password: String,
 )
