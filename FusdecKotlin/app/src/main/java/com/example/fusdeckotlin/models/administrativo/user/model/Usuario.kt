e86ec3ae009b7b3f6b558f6450f54ef709bc3244
Julian Rivera(VALHEMSING) <juli.rivera.quintero@gmail.com>
2025-04-18 15:44:29 -0500

    fix: Modifcaciones en el modelo de user, agregue un nuevo campo

--- FusdecKotlin/app/src/main/java/com/example/fusdeckotlin/models/administrativo/user/model/Usuario.kt
+++ FusdecKotlin/app/src/main/java/com/example/fusdeckotlin/models/administrativo/user/model/Usuario.kt
@@ -3,118 +3,33 @@ package com.example.fusdeckotlin.models.administrativo.user.model
 import com.google.gson.annotations.SerializedName
 
 data class Usuario(
-    @SerializedName("_id")
-    private val id : String? = null,
-    @SerializedName("nombreUsuario")
-    private var nombreUsuario: String,
-    @SerializedName("apellidoUsuario")
-    private var apellidoUsuario: String,
-    @SerializedName("numeroDocumento")
-    private var numeroDocumento: String,
-    @SerializedName("correo")
-    private var correo: String,
-    @SerializedName("password")
-    private var password: String,
-    @SerializedName("roles")
-    private var roles: List<String>,
-    @SerializedName("estadoUsuario")
-    private var estadoUsuario: Boolean = true,
-    @SerializedName("")
-    private val creadoEn: String = obtenerFechaActual()
+
+    @SerializedName("numero_Documento") private var numeroDocumento: String,
+    @SerializedName("nombre") private var nombre: String,
+    @SerializedName("apellido") private var apellido: String,
+    @SerializedName("correo") private var correo: String,
+    @SerializedName("password") private var password: String,
+    @SerializedName("estado") private var estado: Boolean = true,
+    @SerializedName("createAt") private val createAt: String,
+    @SerializedName("updateAt") private val updateAt: String
 ) {
 
     /**++++++++++++++GETTERS+++++++++++++++++*/
 
-    fun getUserId(): String = id.toString()
-
-    fun getNombreUsuario(): String = nombreUsuario
-
-    fun getApellidoUsuario(): String = apellidoUsuario
-
     fun getNumeroDocumento(): String = numeroDocumento
+
+    fun getNombreUsuario(): String = nombre
+
+    fun getApellidoUsuario(): String = apellido
+
 
     fun getCorreo(): String = correo
 
     fun getPassword(): String= password
 
-    fun getRoles(): List<String> = roles
+    fun getEstadoUsuario(): Boolean = estado
 
-    fun getEstadoUsuario(): Boolean = estadoUsuario
+    fun getCreadoAt(): String = createAt
 
-    fun getCreadoEn(): String = creadoEn
-
-    /**++++++++++++++SETTERS+++++++++++++++++*/
-
-    fun setNombreUsuario(nuevoNombre: String){
-        nombreUsuario = nuevoNombre
-    }
-
-    fun setApellidoUsuario(nuevoApellido: String){
-        apellidoUsuario = nuevoApellido
-    }
-    fun setNuDocumento(newDocumento: String){
-        numeroDocumento = newDocumento
-    }
-    fun setCorreo(newCorreo: String){
-        correo = newCorreo
-    }
-    fun setPassword(nuevaPassword: String) {
-        password = nuevaPassword
-    }
-
-    fun setRoles(nuevosRoles: List<String>) {
-        roles = nuevosRoles
-    }
-
-    fun setEstadoUsuario(nuevoEstado: Boolean) {
-        estadoUsuario = nuevoEstado
-    }
-
-
-
-    companion object{
-        val administrador = Usuario(
-            nombreUsuario = "Carlos",
-            apellidoUsuario = "Gómez",
-            numeroDocumento = "9876543210",
-            correo = "carlos.gomez@gmail.com",
-            password = "password123",
-            roles = listOf("Administrativo"),
-            estadoUsuario = false,
-            creadoEn = "2023-01-01"
-        )
-
-        val secretario = Usuario(
-            nombreUsuario = "Ana",
-            apellidoUsuario = "Pérez",
-            numeroDocumento = "1234567890",
-            correo = "ana.perez@gmail.com",
-            password = "password456",
-            roles = listOf("Secretario"),
-            estadoUsuario = true,
-            creadoEn = "2023-01-02"
-        )
-
-        val instructor = Usuario(
-            nombreUsuario = "Luis",
-            apellidoUsuario = "Martínez",
-            numeroDocumento = "5555555555",
-            correo = "luis.martinez@gmail.com",
-            password = "password789",
-            roles = listOf("Instructor"),
-            estadoUsuario = true,
-            creadoEn = "2023-01-03"
-        )
-        fun generateId(): String{
-            return java.util.UUID.randomUUID().toString()
-        }
-
-        fun obtenerFechaActual(): String{
-            val formato = java.text.SimpleDateFormat("yyyy-MM-dd")
-
-            return formato.format(java.util.Date())
-        }
-    }
-
-
+    fun getUpdateAt(): String = updateAt
 }
