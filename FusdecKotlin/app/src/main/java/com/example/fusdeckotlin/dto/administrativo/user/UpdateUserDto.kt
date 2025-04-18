bfb4ec9ba7473bbb2d005dbef85850179c01487f
Julian Rivera(VALHEMSING) <juli.rivera.quintero@gmail.com>
2025-04-18 15:59:40 -0500

    fix: modificacion de updateUserDto

--- FusdecKotlin/app/src/main/java/com/example/fusdeckotlin/dto/administrativo/user/UpdateUserDto.kt
+++ FusdecKotlin/app/src/main/java/com/example/fusdeckotlin/dto/administrativo/user/UpdateUserDto.kt
@@ -3,16 +3,9 @@ package com.example.fusdeckotlin.dto.administrativo.user
 import com.google.gson.annotations.SerializedName
 
 data class UpdateUserDto(
-    @SerializedName("nombreUsuario")
-    private var nombreUsuario: String?  = null,
-    @SerializedName("apellidoUsuario")
-    private var apellidoUsuario: String?  = null,
-    @SerializedName("numeroDocumento")
-    private var numeroDocumento: String?  = null,
-    @SerializedName("correo")
-    private var correo: String?  = null,
-    @SerializedName("password")
-    private var password: String?  = null,
-    @SerializedName("roles")
-    private var roles: List<String>?  = null,
+    @SerializedName("numero_documento") private var numeroDocumento: String?,
+    @SerializedName("nombre") private var nombre: String?,
+    @SerializedName("apellido") private var apellido: String?,
+    @SerializedName("correo") private var correo: String?,
+    @SerializedName("password") private var password: String?,
 )
