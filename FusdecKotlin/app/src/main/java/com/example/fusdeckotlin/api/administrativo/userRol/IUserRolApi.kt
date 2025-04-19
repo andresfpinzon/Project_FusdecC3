f7b08c38c33784ec0e4892006798cf4c4be5cf3f
Julian Rivera(VALHEMSING) <juli.rivera.quintero@gmail.com>
2025-04-18 20:04:22 -0500

    feat: Agruegue rutas para role y user

---
+++ FusdecKotlin/app/src/main/java/com/example/fusdeckotlin/api/administrativo/userRol/IUserRolApi.kt
@@ -0,0 +1,22 @@
+package com.example.fusdeckotlin.api.administrativo.userRol
+
+import com.example.fusdeckotlin.dto.administrativo.userRol.AddRolUserDto
+import com.example.fusdeckotlin.models.administrativo.userRol.UserRolModel
+import retrofit2.Response
+import retrofit2.http.*
+
+interface IUserRolApi {
+
+    @GET("roles")
+    suspend fun getRolesOfUsers(): Response<List<UserRolModel>>
+
+    @POST("roles")
+    suspend fun AddRolToUser(@Body data: AddRolUserDto): Response<UserRolModel>
+
+    @GET("roles/{document}")
+    suspend fun getRolByDocumentUser(@Path("document") document: String): Response<UserRolModel>
+
+    @DELETE("roles/{document}/{rol}")
+    suspend fun deleteRolToUser(@Path("document") document: String, @Path("rol") rol: String ): Response<UserRolModel>
+
+}
