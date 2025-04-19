251d330e7cac2d8da9513c3479a833c60d73493d
Julian Rivera(VALHEMSING) <juli.rivera.quintero@gmail.com>
2025-04-18 20:16:54 -0500

    feat: Agruegue archivo que se encargara de la logica de negocios finalizado

--- FusdecKotlin/app/src/main/java/com/example/fusdeckotlin/services/administrativo/userRolServices/UserRolServices.kt
+++ FusdecKotlin/app/src/main/java/com/example/fusdeckotlin/services/administrativo/userRolServices/UserRolServices.kt
@@ -1,4 +1,48 @@
 package com.example.fusdeckotlin.services.administrativo.userRolServices
 
+import com.example.fusdeckotlin.api.administrativo.userRol.IUserRolApi
+import com.example.fusdeckotlin.config.retrofit.RetrofitClient
+import com.example.fusdeckotlin.dto.administrativo.userRol.AddRolUserDto
+import com.example.fusdeckotlin.models.administrativo.userRol.UserRolModel
+import com.example.fusdeckotlin.utils.ResponseHandler.handleListResponse
+import com.example.fusdeckotlin.utils.ResponseHandler.handleResponse
+
 class UserRolServices {
+
+    private val userRolApi: IUserRolApi = RetrofitClient.userRolApi
+
+    suspend fun getRolesOfUser(): Result<List<UserRolModel>>{
+        return try {
+            val res = userRolApi.getRolesOfUsers()
+            handleListResponse(res)
+        }catch (e: Exception){
+            Result.failure(e)
+        }
+    }
+    suspend fun addRolToUser(data: AddRolUserDto): Result<UserRolModel>{
+        return try {
+            val res = userRolApi.AddRolToUser(data)
+            handleResponse(res)
+        }catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
+
+    suspend fun getRoleByUser(document: String): Result<UserRolModel> {
+        return try {
+            val res = userRolApi.getRolByDocumentUser(document)
+            handleResponse(res)
+        }catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
+
+    suspend fun delteRoleOfUser(document: String, role: String): Result<UserRolModel>{
+        return  try {
+            val res = userRolApi.deleteRolToUser(document, role)
+            handleResponse(res)
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
 }
