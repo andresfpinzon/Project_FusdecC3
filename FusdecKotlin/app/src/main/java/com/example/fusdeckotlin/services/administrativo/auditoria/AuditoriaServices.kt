1de837b81589be107cbb5e1cb3e2a53973c64e7a
Julian Rivera(VALHEMSING) <juli.rivera.quintero@gmail.com>
2025-04-20 17:49:38 -0500

    fix: Modificacion de la logica de negocios de auditoria

--- FusdecKotlin/app/src/main/java/com/example/fusdeckotlin/services/administrativo/auditoria/AuditoriaServices.kt
+++ FusdecKotlin/app/src/main/java/com/example/fusdeckotlin/services/administrativo/auditoria/AuditoriaServices.kt
@@ -20,42 +20,42 @@ class AuditoriaServices {
     }
 
 
-    suspend fun getAuditoria(id: String): Result<Auditoria>{
-        return try {
-            val res = auditoriaApi.getAuditoriaById(id)
-            handleResponse(res)
-        } catch (e: Exception){
-            Result.failure(e)
-        }
-    }
-
-    suspend fun getAuditoriaByCertificadoId(id: String): Result<Auditoria>{
-        return  try {
-            val res = auditoriaApi.getAuditoriaByCertificadoId(id)
-            handleResponse(res)
-        } catch (e: Exception){
-            Result.failure(e)
-        }
-    }
-
-//    companion object{
-//        fun createAuditoria(auditoriaDB: MutableList<AuditoriaModel>, data: AuditoriaModel): AuditoriaModel{
-//            val newAuditoria = AuditoriaModel(
-//                nombreEmisor = data.getNombreEmisorAuditoria(),
-//            )
-//
-//            auditoriaDB.add(newAuditoria)
-//            return newAuditoria
-//        }
-//
-//        fun removeAuditoriaById(auditoriaDB: MutableList<AuditoriaModel>, id: String): Boolean{
-//            val auditoria = auditoriaDB.find { it.getIdAuditoria() == id }
-//            return if(auditoria != null){
-//                auditoriaDB.remove(auditoria)
-//            }else {
-//                false
-//            }
+//    suspend fun getAuditoria(id: String): Result<Auditoria>{
+//        return try {
+//            val res = auditoriaApi.getAuditoriaById(id)
+//            handleResponse(res)
+//        } catch (e: Exception){
+//            Result.failure(e)
 //        }
 //    }
+//
+//    suspend fun getAuditoriaByCertificadoId(id: String): Result<Auditoria>{
+//        return  try {
+//            val res = auditoriaApi.getAuditoriaByCertificadoId(id)
+//            handleResponse(res)
+//        } catch (e: Exception){
+//            Result.failure(e)
+//        }
+//    }
+//
+////    companion object{
+////        fun createAuditoria(auditoriaDB: MutableList<AuditoriaModel>, data: AuditoriaModel): AuditoriaModel{
+////            val newAuditoria = AuditoriaModel(
+////                nombreEmisor = data.getNombreEmisorAuditoria(),
+////            )
+////
+////            auditoriaDB.add(newAuditoria)
+////            return newAuditoria
+////        }
+////
+////        fun removeAuditoriaById(auditoriaDB: MutableList<AuditoriaModel>, id: String): Boolean{
+////            val auditoria = auditoriaDB.find { it.getIdAuditoria() == id }
+////            return if(auditoria != null){
+////                auditoriaDB.remove(auditoria)
+////            }else {
+////                false
+////            }
+////        }
+////    }
 
 }
