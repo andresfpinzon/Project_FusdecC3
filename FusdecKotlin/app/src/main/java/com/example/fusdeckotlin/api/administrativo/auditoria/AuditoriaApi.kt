e349f80a382a6bd2b1823633dceadddde1b39316
Julian Rivera(VALHEMSING) <juli.rivera.quintero@gmail.com>
2025-04-20 17:45:48 -0500

    fix: Modificacion del ruta de auditoria

--- FusdecKotlin/app/src/main/java/com/example/fusdeckotlin/api/administrativo/auditoria/AuditoriaApi.kt
+++ FusdecKotlin/app/src/main/java/com/example/fusdeckotlin/api/administrativo/auditoria/AuditoriaApi.kt
@@ -7,18 +7,18 @@ import retrofit2.Response
 
 interface AuditoriaApi {
 
-    @GET("api/auditorias")
+    @GET("auditorias")
     suspend fun getAuditorias(): Response<List<Auditoria>>
 
-    @POST("api/auditorias")
-    suspend fun createAuditoria(data: CreateAuditoriaDto): Response<Auditoria>
-
-    @GET("api/auditorias/{id}")
-    suspend fun getAuditoriaById(@Path("id") id: String): Response<Auditoria>
-
-    @GET("api/auditorias/certificado/{certificadoId}")
-    suspend fun getAuditoriaByCertificadoId(@Path("certificadoId") certificadoId: String): Response<Auditoria>
-
-    @DELETE("api/auditorias/{id}")
-    suspend fun deleteAuditoriaById(@Path("id") id: String): Response<Auditoria>
+//    @POST("api/auditorias")
+//    suspend fun createAuditoria(data: CreateAuditoriaDto): Response<Auditoria>
+//
+//    @GET("api/auditorias/{id}")
+//    suspend fun getAuditoriaById(@Path("id") id: String): Response<Auditoria>
+//
+//    @GET("api/auditorias/certificado/{certificadoId}")
+//    suspend fun getAuditoriaByCertificadoId(@Path("certificadoId") certificadoId: String): Response<Auditoria>
+//
+//    @DELETE("api/auditorias/{id}")
+//    suspend fun deleteAuditoriaById(@Path("id") id: String): Response<Auditoria>
 }
