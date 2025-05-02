e20dea6eb67bbe973cc6fb014d917c70a74b7e9b
Julian Rivera(VALHEMSING) <juli.rivera.quintero@gmail.com>
2025-05-02 13:59:33 -0500

    feat: Agregue los campos de 'Edicion'

--- kotlinsql/src/main/kotlin/com/example/kotlinsql/model/edicion/Edicion.kt
+++ kotlinsql/src/main/kotlin/com/example/kotlinsql/model/edicion/Edicion.kt
@@ -1,3 +1,30 @@
 package com.example.kotlinsql.model.edicion
 
-data class Edicion()
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
+import java.time.LocalDate
+
+@Entity
+@Table(name = "edicion")
+data class Edicion(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id_edicion: Long = 0,
+
+    val titulo: String,
+
+    val fechaInicio: LocalDate,
+
+    val fechaFin: LocalDate,
+
+    @ManyToOne
+    @JoinColumn(name = "curso_id", nullable = true)
+    val curso: Int? = null,
+
+    val estado: Boolean = true
+)
