054d32da9bc21331a055139ec7cdb7d62a520f91
Julian Rivera(VALHEMSING) <juli.rivera.quintero@gmail.com>
2025-05-02 13:45:38 -0500

    feat: Agregue servicio de crear una  Unidad

--- kotlinsql/src/main/kotlin/com/example/kotlinsql/services/unidad/UnidadServices.kt
+++ kotlinsql/src/main/kotlin/com/example/kotlinsql/services/unidad/UnidadServices.kt
@@ -1,4 +1,17 @@
 package com.example.kotlinsql.services.unidad
 
+import com.example.kotlinsql.dto.unidad.CreateUnidadDto
+import com.example.kotlinsql.model.unidad.Unidad
+
 class UnidadServices {
+    fun crearUnidadService(data: CreateUnidadDto): Unidad {
+
+        val unidad = Unidad(
+            nombreUnidad = data.nombreUnidad,
+            brigadaId = data.brigadaId,
+            usuarioId = data.usuarioId,
+        )
+
+        return unidadRepository.save(unidad)
+    }
 }
