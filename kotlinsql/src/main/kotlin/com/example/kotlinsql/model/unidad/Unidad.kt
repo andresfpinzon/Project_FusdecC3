23ea2a6c193a37ce60dd7c3a7e924070df3660e2
Julian Rivera(VALHEMSING) <juli.rivera.quintero@gmail.com>
2025-05-02 13:40:01 -0500

    feat: Agregue los campos de Unidad

--- kotlinsql/src/main/kotlin/com/example/kotlinsql/model/unidad/Unidad.kt
+++ kotlinsql/src/main/kotlin/com/example/kotlinsql/model/unidad/Unidad.kt
@@ -1,5 +1,26 @@
 package com.example.kotlinsql.model.unidad
 
-data class Unidad(
+import jakarta.persistence.*
 
-)
+@Entity
+@Table(name = "unidad")
+data class Unidad(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_unidad")
+    val idUnidad: Long? = null,
+
+    @Column(name = "nombre_unidad")
+    val nombreUnidad: String,
+
+    @Column(name = "estado_unidad")
+    val estadoUnidad: Boolean = true,
+
+    @Column(name = "brigada_id", nullable = true)
+    val brigadaId: Int? = null,
+
+    @Column(name = "usuario_id")
+    val usuarioId: String
+) {
+    constructor() : this(null, "", true, null, "")
+}
