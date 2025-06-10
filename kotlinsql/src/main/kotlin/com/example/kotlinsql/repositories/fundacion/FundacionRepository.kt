66ad54c5f42a50ea4351f3244d24c4f2eedb56e9
Julian Rivera <juli.rivera.quintero@gmail.com>
2025-06-09 23:29:39 -0500

    feat: Agrego reposiotorio, con tipado para acceder metodos nativos

---
+++ kotlinsql/src/main/kotlin/com/example/kotlinsql/repositories/fundacion/FundacionRepository.kt
@@ -0,0 +1,9 @@
+package com.example.kotlinsql.repositories.fundacion
+
+import com.example.kotlinsql.model.edicion.Edicion
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.stereotype.Repository
+
+interface FundacionRepository {
+}
