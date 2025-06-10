f8502c3d93995f5a220b0e023ec588fe0b5c94b3
Julian Rivera <juli.rivera.quintero@gmail.com>
2025-06-09 23:55:32 -0500

    feat: Agrego una nueva query

--- kotlinsql/src/main/kotlin/com/example/kotlinsql/repositories/fundacion/FundacionRepository.kt
+++ kotlinsql/src/main/kotlin/com/example/kotlinsql/repositories/fundacion/FundacionRepository.kt
@@ -1,10 +1,14 @@
 package com.example.kotlinsql.repositories.fundacion
 
 import com.example.kotlinsql.model.edicion.Edicion
+import com.example.kotlinsql.model.fundacion.Fundacion
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
-interface FundacionRepository : JpaRepository<Edicion, Long> {
+interface FundacionRepository : JpaRepository<Fundacion, Long> {
+
+    @Query("SELECT COUNT(f) > 0 FROM fundacion f WHERE LOWER(f.nombre) = LOWER(:nombre)")
+    fun existFundacionByName(nombre: String) : Boolean
 }
