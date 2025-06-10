e16d7129f8943bbea277b1c2e0f55a716ed02756
Julian Rivera <juli.rivera.quintero@gmail.com>
2025-06-09 21:48:29 -0500

    feat: modificacion de las apis de brigada, para que queden acorde  a las rutas de spring

--- FusdecKotlin/app/src/main/java/com/example/fusdeckotlin/api/administrativo/brigada/BrigadaApi.kt
+++ FusdecKotlin/app/src/main/java/com/example/fusdeckotlin/api/administrativo/brigada/BrigadaApi.kt
@@ -7,21 +7,21 @@ import retrofit2.Response
 import retrofit2.http.*
 
 interface BrigadaApi {
-    @GET("api/brigadas")
+    @GET("/brigadas")
     suspend fun listarBrigadas(): Response<List<Brigada>>
 
-    @POST("api/brigadas")
+    @POST("/brigadas")
     suspend fun crearBrigada(@Body request: CrearBrigadaRequest): Response<Brigada>
 
-    @PUT("api/brigadas/{id}")
+    @PUT("/brigadas/{id}")
     suspend fun actualizarBrigada(
         @Path("id") id: String,
         @Body request: ActualizarBrigadaRequest
     ): Response<Brigada>
 
-    @DELETE("api/brigadas/{id}")
+    @DELETE("/brigadas/{id}")
     suspend fun desactivarBrigada(@Path("id") id: String): Response<Brigada>
 
-    @GET("api/brigadas/{id}")
+    @GET("/brigadas/{id}")
     suspend fun obtenerBrigadaPorId(@Path("id") id: String): Response<Brigada>
 }
