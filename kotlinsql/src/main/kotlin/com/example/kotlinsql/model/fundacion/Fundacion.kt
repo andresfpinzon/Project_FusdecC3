00f5d4a47f1803113c4d5f87a879376a74e1eae2
Julian Rivera <juli.rivera.quintero@gmail.com>
2025-06-10 14:53:14 -0500

    fix: modifico el serializedname, con eso ya se ven los nombres al seleccionar las fundaciones

--- kotlinsql/src/main/kotlin/com/example/kotlinsql/model/fundacion/Fundacion.kt
+++ kotlinsql/src/main/kotlin/com/example/kotlinsql/model/fundacion/Fundacion.kt
@@ -1,6 +1,10 @@
 package com.example.kotlinsql.model.fundacion
 
+import jakarta.persistence.*
+
+@Entity
 data class Fundacion(
+    @Id
     val id: Long? = null,
     val nombre: String,
     val estado: Boolean = true
