533def85d77c1613caaada5fcecab6767eaf71ad
Julian Rivera <juli.rivera.quintero@gmail.com>
2025-06-09 23:56:00 -0500

    feat: Agrego una nueva servicio de crear

--- kotlinsql/src/main/kotlin/com/example/kotlinsql/services/fundacion/FundacionService.kt
+++ kotlinsql/src/main/kotlin/com/example/kotlinsql/services/fundacion/FundacionService.kt
@@ -1,28 +1,30 @@
 package com.example.kotlinsql.services.fundacion
 
+import com.example.kotlinsql.dto.fundacion.FundacionCreateDto
 import com.example.kotlinsql.model.fundacion.Fundacion
+import com.example.kotlinsql.repositories.fundacion.FundacionRepository
+import org.springframework.dao.DataIntegrityViolationException
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.jdbc.core.RowMapper
 import org.springframework.stereotype.Service
 
 @Service
-class FundacionService(private val jdbcTemplate: JdbcTemplate) {
+class FundacionService(
+    private val fundacionRepository: FundacionRepository
+) {
 
-    val rowMapper = RowMapper<Fundacion> { rs, _ ->
-        Fundacion(
-            id = rs.getInt("id"),
-            nombre = rs.getString("nombre"),
-            estado = rs.getBoolean("estado"),
-        )
-    }
-
-    fun obtenerTodas(): List<Fundacion> {
-        return jdbcTemplate.query("SELECT * FROM  fundacion", rowMapper)
-    }
-
-    fun obtenerPorId(id: Int): Fundacion? {
-        val sql = "SELECT * FROM fundacion WHERE id = ?"
-        return jdbcTemplate.queryForObject(sql, rowMapper, id)
+    fun createFundacion(data: FundacionCreateDto): Fundacion{
+        return try {
+            if (fundacionRepository.existFundacionByName(data.nombre)) {
+                throw IllegalArgumentException("Ya existe una fundación con ese nombre")
+            }
+            val fundacion = Fundacion(
+                nombre = data.nombre
+            )
+            fundacionRepository.save(fundacion)
+        } catch (e: DataIntegrityViolationException) {
+            throw IllegalArgumentException("Error al crear la Fundacion: ${e.message}")
+        }
     }
 
 }
