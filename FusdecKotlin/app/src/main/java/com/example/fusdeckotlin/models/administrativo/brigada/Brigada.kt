b1fdf8dcffb6246ab927b4322e182d18dcc25b1a
Julian Rivera <juli.rivera.quintero@gmail.com>
2025-06-09 22:01:16 -0500

    fix: modifico el modelo de brigada, ya no espera unidades

--- FusdecKotlin/app/src/main/java/com/example/fusdeckotlin/models/administrativo/brigada/Brigada.kt
+++ FusdecKotlin/app/src/main/java/com/example/fusdeckotlin/models/administrativo/brigada/Brigada.kt
@@ -15,8 +15,8 @@ data class Brigada(
     private var estadoBrigada: Boolean = true,
     @SerializedName("comandoId")
     private var comandoId: Any,
-    @SerializedName("unidades")
-    private var unidades: List<Any> = emptyList()
+    //@SerializedName("unidades")
+    //private var unidades: List<Any> = emptyList()
 ) {
     // Getters básicos
     fun getId() = id.toString()
@@ -65,35 +65,35 @@ data class Brigada(
         )
     }
 
-    fun getUnidades(): List<Unidad> {
-        return unidades.mapNotNull {
-            when (it) {
-                is Unidad -> it
-                is String -> Unidad(id = it, "", "", true, "")
-                is Map<*, *> -> convertMaptoUnidades(it)
-                else -> null
-            }
-        }
-    }
-    fun getUnidadesIds(): List<String>{
-        return  unidades.map{
-            when(it) {
-                is Unidad -> it.getId() ?: ""
-                is String -> it
-                is Map<*, *> -> it["_id"] as? String ?: ""
-                else -> ""
-            }
-        }.filter { it.isNotEmpty() }
-    }
-
-    private fun convertMaptoUnidades(map: Map<*, *>): Unidad {
-        return Unidad (
-            id = map["_id"] as? String ?: "",
-            nombreUnidad = map["nombreUnidad"] as? String ?: "",
-            brigadaId = map["brigadaId"] as? String ?: "",
-            estadoUnidad = map["estadoUnidad"] as? Boolean ?: true,
-            usuarioId = map["usuarioId"] as? String ?: "",
-        )
-    }
+//    fun getUnidades(): List<Unidad> {
+//        return unidades.mapNotNull {
+//            when (it) {
+//                is Unidad -> it
+//                is String -> Unidad(id = it, "", "", true, "")
+//                is Map<*, *> -> convertMaptoUnidades(it)
+//                else -> null
+//            }
+//        }
+//    }
+//    fun getUnidadesIds(): List<String>{
+//        return  unidades.map{
+//            when(it) {
+//                is Unidad -> it.getId() ?: ""
+//                is String -> it
+//                is Map<*, *> -> it["_id"] as? String ?: ""
+//                else -> ""
+//            }
+//        }.filter { it.isNotEmpty() }
+//    }
+//
+//    private fun convertMaptoUnidades(map: Map<*, *>): Unidad {
+//        return Unidad (
+//            id = map["_id"] as? String ?: "",
+//            nombreUnidad = map["nombreUnidad"] as? String ?: "",
+//            brigadaId = map["brigadaId"] as? String ?: "",
+//            estadoUnidad = map["estadoUnidad"] as? Boolean ?: true,
+//            usuarioId = map["usuarioId"] as? String ?: "",
+//        )
+//    }
 
 }
