db240fc73f822c853f425522848683fa6b5afcfd
Julian Rivera <juli.rivera.quintero@gmail.com>
2025-06-09 23:31:28 -0500

    feat: tipo la interfaz

--- kotlinsql/src/main/kotlin/com/example/kotlinsql/repositories/fundacion/FundacionRepository.kt
+++ kotlinsql/src/main/kotlin/com/example/kotlinsql/repositories/fundacion/FundacionRepository.kt
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
-interface FundacionRepository {
+@Repository
+interface FundacionRepository : JpaRepository<Edicion, Long> {
 }
