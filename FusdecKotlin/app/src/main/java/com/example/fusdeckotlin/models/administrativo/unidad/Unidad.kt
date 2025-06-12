aebd929abc802ed7dbc641f009fe1bcddb5da69f
Julian Rivera <juli.rivera.quintero@gmail.com>
2025-06-11 23:40:35 -0500

    fix: modelo de unidad corregido

--- FusdecKotlin/app/src/main/java/com/example/fusdeckotlin/models/administrativo/unidad/Unidad.kt
+++ FusdecKotlin/app/src/main/java/com/example/fusdeckotlin/models/administrativo/unidad/Unidad.kt
@@ -5,98 +5,21 @@ import com.google.gson.annotations.SerializedName
 import com.example.fusdeckotlin.models.administrativo.user.model.Usuario
 
 data class Unidad(
-    @SerializedName("id_unidad")
-    private val id: String,
-    @SerializedName("nombre_unidad")
+    @SerializedName("id")
+    private val id: Long? = null,
+    @SerializedName("nombreUnidad")
     private var nombreUnidad: String,
-    @SerializedName("brigada_Id")
-    private var brigadaId: Any,
-    @SerializedName("estado_unidad")
+    @SerializedName("brigadaId")
+    private var brigadaId: Int,
+    @SerializedName("estadoUnidad")
     private var estadoUnidad: Boolean = true,
-    @SerializedName("usuario_id")
-    private var usuarioId: Any,
+    @SerializedName("usuarioId")
+    private var usuarioId: String,
 ) {
     fun getId() = id
     fun getNombreUnidad() = nombreUnidad
-
-    fun getBrigadaId(): String{
-        return when (brigadaId) {
-            is String -> brigadaId as String
-            is Brigada -> (brigadaId as Brigada).getId()
-            is Map<*, *> -> (brigadaId as Map<*, *>)["_id"] as? String ?: ""
-            else -> ""
-        }
-    }
-
-    fun getBrigada(): Brigada {
-        return when (brigadaId){
-            is Brigada -> brigadaId as Brigada
-            is String -> createBrigradaEmpty(brigadaId as String)
-            is Map<*,*> -> convertMaptoBrigada(brigadaId as Map<*,*>)
-            else -> createBrigradaEmpty("")
-        }
-    }
-
-    private fun createBrigradaEmpty(id: String): Brigada{
-         return Brigada(
-             id = id,
-             nombreBrigada = "",
-             ubicacionBrigada = "",
-             comandoId = "",
-             //unidades = emptyList(),
-         )
-    }
-
-    private fun convertMaptoBrigada(map: Map<*,*>): Brigada {
-        return Brigada(
-            id = map["_id"] as? String ?: "",
-            nombreBrigada = map["nombreBrigada"] as? String ?: "",
-            ubicacionBrigada = map["ubicacionBrigada"] as? String ?: "",
-            comandoId = map["comandoId"] as? String ?: "",
-            //unidades = map["unidades"] as? List<String> ?: emptyList()
-        )
-    }
+    fun getBrigadaId() = brigadaId
     fun getEstadoUnidad() = estadoUnidad
-
-    fun getUsuarioId(): String {
-        return when (usuarioId) {
-            is String -> usuarioId as String
-            is Usuario -> (usuarioId as Usuario).getNumeroDocumento()
-            is Map<*,*> -> (usuarioId as Map<*,*>) ["_id"] as? String ?:""
-            else -> ""
-        }
-    }
-    fun getUser(): Usuario {
-        return when (usuarioId){
-            is Usuario -> usuarioId as Usuario
-            is String -> createUsersEmpty(usuarioId as String)
-            is Map<*,*> -> convertMapToUsers(usuarioId as Map<*,*>)
-            else -> createUsersEmpty("")
-        }
-    }
-
-    private fun createUsersEmpty(numeroDocumento: String): Usuario {
-        return Usuario(
-            numeroDocumento = numeroDocumento,
-            nombre = "",
-            apellido = "",
-            correo = "",
-            password = "",
-            createAt = "",
-            updateAt = ""
-        )
-    }
-
-    private fun convertMapToUsers(map: Map<*,*>): Usuario {
-        return Usuario(
-            numeroDocumento = map["numero_documento"] as? String ?: "",
-            nombre = map["nombre"] as? String ?: "",
-            apellido = map["apellido"] as? String ?: "",
-            correo = map["correo"] as? String ?: "",
-            password = map["password"] as? String ?: "",
-            createAt = map["create_at"] as? String ?: "",
-            updateAt = map["update_at"]  as? String ?: ""
-        )
-    }
+    fun getUsuarioId() = usuarioId
 
 }
